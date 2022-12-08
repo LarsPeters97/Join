8506a43975d14082e0425814d24179cf5379e146
Marcel Kuepper <marcelk95@hotmail.de>
2022-12-08 14:50:12 +0100

    added bord headline and bord js structure

---
+++ js/board.js
@@ -0,0 +1,32 @@
+let todos = [];
+let inProgress = [];
+let awaitFeedback = [];
+let doneTasks = [];
+
+function initBoard() {
+    loadTodos();
+    loadInProgress();
+    loadAwaitFeedback();
+    loadDoneTasks();
+    renderBoard();
+}
+
+async function loadTodos(){
+
+}
+
+async function loadInProgress(){
+
+}
+
+async function loadAwaitFeedback(){
+
+}
+
+async function loadDoneTasks() {
+
+}
+
+function renderBoard(){
+    
+}
