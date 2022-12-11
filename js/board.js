35674a809c0e5b167ceef3530c839b79b5b7de13
Marcel Kuepper <marcelk95@hotmail.de>
2022-12-11 01:45:39 +0100

    drag-and-drop function added

--- js/board.js
+++ js/board.js
@@ -1,16 +1,36 @@
-let tasklist= [];
+let tasklist = [{
+    'progress': 'todo',
+    'id': 0,
+    'category': 'Design',
+    'title': 'Website redesign',
+    'description': 'lorem ipsum',
+    'subtasks': ['lorem ipsum', 'lorem ipsum duo'],
+    'completedtasks': ['lorem ipsum'],
+    'assignedToIco': ['me', 'MK'],
+    'assignetTo': ['me', 'Marcel Küpper'],
+    'priority': 'high',
+}];
 let todos = [];
 let inProgress = [];
 let awaitFeedback = [];
 let doneTasks = [];
+let currentDraggedElement;
 
 async function initBoard() {
-    await loadTasklist()
+    await loadAll()
+    renderBoard();
+}
+
+async function loadAll(){
+    /**await loadTasklist()*/
     loadTodos();
     loadInProgress();
     loadAwaitFeedback();
     loadDoneTasks();
-    renderBoard();
+}
+
+async function save(){
+
 }
 
 async function loadTasklist(){
@@ -18,19 +38,19 @@ async function loadTasklist(){
 }
 
 function loadTodos(){
-    todos = tasklist.filter(t => t['progress'] == 'toDo');
+    todos = tasklist.filter(t => t['progress'] == 'todo');
 }
 
 function loadInProgress(){
-    inProgress = tasklist.filter(t => t['progress'] == 'inProgress');
+    inProgress = tasklist.filter(t => t['progress'] == 'inprogresss');
 }
 
 function loadAwaitFeedback(){
-    awaitFeedback = tasklist.filter(t => t['progress'] == 'awaitFeedback');
+    awaitFeedback = tasklist.filter(t => t['progress'] == 'awaitfeedback');
 }
 
 function loadDoneTasks() {
-    doneTasks = tasklist.filter(t => t['progress'] == 'doneTask');
+    doneTasks = tasklist.filter(t => t['progress'] == 'donetask');
 }
 
 function renderBoard(){
@@ -41,7 +61,7 @@ function renderBoard(){
 }
 
 function renderTodos(){
-    document.getElementById('toDo').innerHTML = ``;
+    document.getElementById('toDos').innerHTML = ``;
     for (let i = 0; i < todos.length; i++) {
         let toDo = todos[i];
         let id = toDo['id'];
@@ -52,12 +72,13 @@ function renderTodos(){
         let completedtasks = toDo['completedtasks'].length;
         let assignedIconToThree = assignedTo(toDo['assignedToIco']);
         let priority = toDo['priority'];
-        document.getElementById('toDo').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
+        document.getElementById('toDos').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
     }
+    document.getElementById('toDos').innerHTML += addDragarea('todo');
 }
 
 function renderInProgress(){
-    document.getElementById('toDo').innerHTML = ``;
+    document.getElementById('inProgress').innerHTML = ``;
     for (let i = 0; i < inProgress.length; i++) {
         let toDo = inProgress[i];
         let id = toDo['id'];
@@ -68,12 +89,13 @@ function renderInProgress(){
         let completedtasks = toDo['completedtasks'].length;
         let assignedIconToThree = assignedTo(toDo['assignedToIco']);
         let priority = toDo['priority'];
-        document.getElementById('toDo').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
+        document.getElementById('inProgress').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
     }
+    document.getElementById('inProgress').innerHTML += addDragarea('inprogresss');
 }
 
 function renderAwaitFeedback(){
-    document.getElementById('toDo').innerHTML = ``;
+    document.getElementById('awaitingFeedback').innerHTML = ``;
     for (let i = 0; i < awaitFeedback.length; i++) {
         let toDo = awaitFeedback[i];
         let id = toDo['id'];
@@ -84,12 +106,13 @@ function renderAwaitFeedback(){
         let completedtasks = toDo['completedtasks'].length;
         let assignedIconToThree = assignedTo(toDo['assignedToIco']);
         let priority = toDo['priority'];
-        document.getElementById('toDo').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
+        document.getElementById('awaitingFeedback').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
     }
+    document.getElementById('awaitingFeedback').innerHTML += addDragarea('awaitfeedback');
 }
 
 function renderDoneTasks(){
-    document.getElementById('toDo').innerHTML = ``;
+    document.getElementById('doneTasks').innerHTML = ``;
     for (let i = 0; i < doneTasks.length; i++) {
         let toDo = doneTasks[i];
         let id = toDo['id'];
@@ -100,8 +123,9 @@ function renderDoneTasks(){
         let completedtasks = toDo['completedtasks'].length;
         let assignedIconToThree = assignedTo(toDo['assignedToIco']);
         let priority = toDo['priority'];
-        document.getElementById('toDo').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
+        document.getElementById('doneTasks').innerHTML += toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority)
     }
+    document.getElementById('doneTasks').innerHTML += addDragarea('donetask');
 }
 
 function assignedTo(assignedToIco){
@@ -114,12 +138,41 @@ function assignedTo(assignedToIco){
 }
 
 function toDoTemplate(id, category, title, description, subtasks, completedtasks, assignedIconToThree, priority){
-    return html`
-    <div class="todo" ondragstart="startDragging(${id})">
+    return `
+    <div class="todo" draggable=true ondragstart="startDragging(${id})">
         <div class="category${category}">${category}</div>
         <div class="title">${title}</div>
         <div class="description">${description}</div>
         <div class="subtaskbar">${completedtasks}/${subtasks} Done</div>
         <div class="todofooter"><div>${assignedIconToThree}</div><div>${priority}</div></div>
     </div>`
+}
+
+function addDragarea(id){
+    return `
+    <div id="${id}" class="dragarea" ondrop="drop('${id}')" ondragover="allowDrop(event); highlight('${id}')" ondragleave="removeHighlight('${id}')"></div>
+    `
+}
+
+function startDragging(id){
+    currentDraggedElement = id;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drop(destination){
+    tasklist[currentDraggedElement]['progress'] = destination;
+    /**save();*/
+    loadAll();
+    renderBoard();
+}
+
+function highlight(id){
+    document.getElementById(id).classList.add('dragarea-highlight');
+}
+
+function removeHighlight(id){
+    document.getElementById(id).classList.remove('dragarea-highlight');
 }
