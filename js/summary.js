ae479aa70c190ab6d3a49975f2d2531172907a98
MaximilianLeyh <118059860+MaximilianLeyh@users.noreply.github.com>
2022-12-16 16:29:25 +0100

    added summary.js functions for filtering

--- js/summary.js
+++ js/summary.js
@@ -1,12 +1,215 @@
+let todos = [];
+let inProgress = [];
+let awaitFeedback = [];
+let doneTasks = [];
+let urgentTasks = [];
+
+let tasklist =
+    [
+        {
+            'progress': 'todo',
+            'id': 0,
+            'category': {
+                'color': 'orange',
+                'categoryName': 'Design',
+            },
+            'duedate': 20230114,
+            'title': 'Website redesign',
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro',
+            'subtasks': {
+                'tasks': [
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': true,
+                    },
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': false,
+                    },
+                ],
+            },
+            'assignedTo': {
+                'user': [
+                    {
+                        'name': 'me',
+                        'icon': 'ME',
+                        'iconcolor': '#0190E0',
+                    },
+                    {
+                        'name': 'Marcel Küpper',
+                        'icon': 'MK',
+                        'iconcolor': '#02CF2F',
+                    },
+                    {
+                        'name': 'You',
+                        'icon': 'Yo',
+                        'iconcolor': '#FF5C00',
+                    },
+                    {
+                        'name': 'Someone Else',
+                        'icon': 'SE',
+                        'iconcolor': '#9327FF',
+                    },
+                    {
+                        'name': 'You',
+                        'icon': 'Yo',
+                        'iconcolor': '#FF5C00',
+                    },
+                    {
+                        'name': 'Someone Else',
+                        'icon': 'SE',
+                        'iconcolor': '#9327FF',
+                    },
+                ]},
+                'priority': 'urgent',
+},
+{
+            'progress': 'todo',
+            'id': 1,
+            'category': {
+                'color': 'orange',
+                'categoryName': 'Design',
+            },
+            'duedate': 20221225,
+            'title': 'Website redesign',
+            'description': 'lorem ipsum',
+            'subtasks': {
+                'tasks': [
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': true,
+                    },
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': false,
+                    },
+                    {
+                        'task': 'lorem ipsum duo',
+                        'completed': false,
+                    },
+                    {
+                        'task': 'lorem ipsum duo',
+                        'completed': false,
+                    },
+                ],
+            },
+            'assignedTo': {
+                'user': [
+                    {
+                        'name': 'me',
+                        'icon': 'ME',
+                        'iconcolor': '#0190E0',
+                    },
+                    {
+                        'name': 'Marcel Küpper',
+                        'icon': 'MK',
+                        'iconcolor': '#02CF2F',
+                    },
+                ]},
+                'priority': 'urgent',
+},
+{
+            'progress': 'todo',
+            'id': 1,
+            'category': {
+                'color': 'orange',
+                'categoryName': 'Design',
+            },
+            'duedate': 20221228,
+            'title': 'Website redesign',
+            'description': 'lorem ipsum',
+            'subtasks': {
+                'tasks': [
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': true,
+                    },
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': false,
+                    },
+                    {
+                        'task': 'lorem ipsum duo',
+                        'completed': false,
+                    },
+                    {
+                        'task': 'lorem ipsum duo',
+                        'completed': false,
+                    },
+                ],
+            },
+            'assignedTo': {
+                'user': [
+                    {
+                        'name': 'me',
+                        'icon': 'ME',
+                        'iconcolor': '#0190E0',
+                    },
+                    {
+                        'name': 'Marcel Küpper',
+                        'icon': 'MK',
+                        'iconcolor': '#02CF2F',
+                    },
+                ]},
+                'priority': 'urgent',
+},
+{
+            'progress': 'donetask',
+            'id': 1,
+            'category': {
+                'color': 'orange',
+                'categoryName': 'Design',
+            },
+            'duedate': 20221230,
+            'title': 'Website redesign',
+            'description': 'lorem ipsum',
+            'subtasks': {
+                'tasks': [
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': true,
+                    },
+                    {
+                        'task': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias magnam saepe laborum molestiae quod, sapiente, fugit atque enim repudiandae sunt, cumque totam quaerat commodi praesentium fuga? Non dolore ipsam porro.',
+                        'completed': false,
+                    },
+                    {
+                        'task': 'lorem ipsum duo',
+                        'completed': false,
+                    },
+                    {
+                        'task': 'lorem ipsum duo',
+                        'completed': false,
+                    },
+                ],
+            },
+            'assignedTo': {
+                'user': [
+                    {
+                        'name': 'me',
+                        'icon': 'ME',
+                        'iconcolor': '#0190E0',
+                    },
+                    {
+                        'name': 'Marcel Küpper',
+                        'icon': 'MK',
+                        'iconcolor': '#02CF2F',
+                    },
+                ]},
+                'priority': 'urgent',
+}];
 
 /**
  * init function when body is loading
  */
     function summaryInit() {
         includeHTML();
-        showGreeting();
-        showActiveUserIcon();
-        loadTasksInformation();
+        loadTodos();
+        loadInProgress();
+        loadAwaitFeedback();
+        loadDoneTasks();
+        loadurgentTasks();
+        loadTotalamount();
 }
 
 
@@ -45,82 +248,37 @@ function returnGreetingSlogan(hours) {
    return greetingSlogan;
 }
 
-/**
- * showing different values of userTasks array
- * @param {JSON} userTasksArray - array with user tasks
- */
-function showTasksInformation(userTasksArray) {
-   showTasksInBoard(userTasksArray);
-   showTasksWithStatus(userTasksArray, 'progress');
-   showTasksWithStatus(userTasksArray, 'feedback');
-   showTasksWithStatus(userTasksArray, 'done');
-   showTasksWithStatus(userTasksArray, 'todo');
-   showTasksWithPriority(userTasksArray, 'Hard');
+function loadTodos() {
+    todos = tasklist.filter(t => t['progress'] == 'todo');
+    document.getElementById('amount-todo').innerHTML = `<b>${todos.length}</b>`;
 }
 
-/**
-* loading tasks informations and creating an local array
-*/
-function loadTasksInformation() {
-   let userTasksIds = userAccounts[activeUser].userTasks;
-   userTasksArray = [];
-   if (userTasksIds.length > 0) {
-       for (let i = 0; i < userTasksIds.length; i++) {
-           const taskId = userTasksIds[i];
-           userTasksArray.push(tasks[taskId]);
-       }
-       showTasksInformation(userTasksArray);
-   }
+function loadInProgress() {
+    inProgress = tasklist.filter(t => t['progress'] == 'inprogresss');
+    document.getElementById('amount-progress').innerHTML = `<b>${inProgress.length}</b>`;
 }
 
-/**
-* showing the number of all tasks in userTaks array
-* @param {JSON} userTasksArray - array with user tasks
-*/
-function showTasksInBoard(userTasksArray) {
-   let numberOfTasks = userTasksArray.length;
-   document.getElementById('info-number-board').innerHTML = numberOfTasks;
+function loadAwaitFeedback() {
+    awaitFeedback = tasklist.filter(t => t['progress'] == 'awaitfeedback');
+    document.getElementById('amount-feedback').innerHTML = `<b>${awaitFeedback.length}</b>`;
 }
 
-/**
-* showing numbers of tasks with specific status
-* @param {JSON} userTasksArray - array with user tasks
-* @param {string} taskStatus - status of task
-*/
-function showTasksWithStatus(userTasksArray, taskStatus) {
-   let filterResult = userTasksArray.filter((t) => t.taskStatus.includes(taskStatus));
-   let numberOfTasks = filterResult.length;
-   document.getElementById(`info-number-${taskStatus}`).innerHTML = numberOfTasks;
+function loadDoneTasks() {
+    doneTasks = tasklist.filter(t => t['progress'] == 'donetask');
+    document.getElementById('amount-done').innerHTML = `<b>${doneTasks.length}</b>`;
 }
 
-/**
-* showing numbers of tasks with specific priotity
-* @param {JSON} userTasksArray - array with user tasks
-* @param {string} taskStatus - status of task
-*/
-function showTasksWithPriority(userTasksArray, taskPriority) {
-   let filterResult = userTasksArray.filter((t) => t.priority.includes(taskPriority));
-   let numberOfTasks = filterResult.length;
-   if (filterResult.length > 0) {
-       document.getElementById(`info-number-${taskPriority}`).innerHTML = numberOfTasks;
-       showTasksWithNextDate(filterResult);
-   }
+function loadurgentTasks() {
+urgentTasks=tasklist.filter(t=>t['progress']!='donetask');
+urgentTasks=urgentTasks.filter(t=>t['priority']=='urgent');
+urgentTasks=urgentTasks.sort((a,b)=>{
+if (a.duedate < b.duedate) {
+return -1;
+}
+});
+document.getElementById('amount-urgent').innerHTML = `<b>${urgentTasks.length}</b>`; 
 }
 
-/**
-* showing the upcoming date of the urgent tasks
-* @param {JSON} filterResult - result of filtering urgent tasks
-*/
-function showTasksWithNextDate(filterResult) {
-   let dateArray = [];
-   let options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-   for (let i = 0; i < filterResult.length; i++) {
-       const taskDate = filterResult[i].toDueDate;
-       let dateObject = new Date(taskDate);
-       dateArray.push(dateObject);
-   }
-   dateArray.sort(function (a, b) {
-       return a - b;
-   });
-   document.getElementById('info-date-deadline').innerHTML = dateArray[0].toLocaleDateString(undefined, options);
+function loadTotalamount() {
+    document.getElementById('amount-total').innerHTML = `<b>${tasklist.length}</b>`;
 }
