95545d72337f0ea631bfaf822e4f65a18df5f76a
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2022-12-16 14:20:05 +0100

    logo animation

--- register.js
+++ register.js
@@ -9,10 +9,11 @@ async function init() {
 }
 
 async function addUser(){
-    let name = document.getElementById('Name_signup');
     let email = document.getElementById('Email_signup');
+    let name = document.getElementById('Name_signup');
     let password = document.getElementById('pasword_signup');
-    users.push({name: name, email: email, password: password});
+    users.push({name: name.value, email: email.value, password: password.value});
     await  backend.setItem('users', JSON.stringify(users));
-    window.location.reload();
+    
+    window.location.href = './index.html?msg=Du hast dich erfolgreich Registriert';
 }
