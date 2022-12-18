5142086ed11f7f8f62d78422a6394099a8754b45
LarsPeters97 <larsar.peterlov@gmail.com>
2022-12-18 12:24:37 +0100

    Category Colors

--- js/add_task.js
+++ js/add_task.js
@@ -7,7 +7,7 @@ let categories = [{
     'color': '#1FD7C1'
 }];
 
-let categoryColors = ['#FC71FF', '#1FD7C1', '#8AA4FF', 'FF0000', '#2AD300', '#FF8A00', '#E200BE', '#0038FF'];
+let categoryColors = ['#FC71FF', '#1FD7C1', '#8AA4FF', '#FF0000', '#2AD300', '#FF8A00', '#E200BE', '#0038FF'];
 
 
 async function init() {
@@ -26,33 +26,8 @@ function showClearImgDarkBlue() {
 }
 
 
-// function createNewCategory() {
-//     document.getElementById('new-category').classList.remove('d-none');
-//     showCategoriesAndInput();
-//     document.getElementById('dropdown-category').innerHTML = 'Create new Category';
-
-// }
-
-
-// function showCategoriesAndInput() {
-//     let dropdownCategory = document.getElementById('dropdown-category');
-//     for(let i = 0; i < categories.length; i++) {
-//         let catgegoryName = categories[i]['name'];
-//         let categoryColor = categories[i]['color'];
-//         dropdownCategory.innerHTML = templateCategories(catgegoryName, categoryColor);
-//     }
-// }
-
-
-// function templateCategories(catgegoryName, categoryColor) {
-//     return /*html*/`
-//     ${catgegoryName} ${categoryColor}`;
-// }
-
-
 function openNewCategoryAndExistingCategories() {
     document.getElementById('new-category').classList.remove('d-none');
-    document.getElementById('category-container').style.borderRadius = '9px 9px 0px 0px';
     let existingCategories = document.getElementById('existing-categories');
     existingCategories.innerHTML = '';
     for (let i = 0; i < categories.length; i++) {
@@ -91,10 +66,33 @@ function createNewCategory() {
     document.getElementById('new-category').style.display = 'none';
     document.getElementById('existing-categories').style.display = 'none';
     document.getElementById('category-container').style.borderRadius = '9px';
+    colorsForNewCategory();
 
     let categoryContainer = document.getElementById('category-container');
-    categoryContainer.innerHTML = /*html*/`
-    <input type="text" placeholder="Enter new category">`;
+    categoryContainer.innerHTML = templateCreateNewCategory();
+}
+
+
+function templateCreateNewCategory() {
+    return /*html*/`
+    <input class="input-category" type="text" placeholder="New Category Name" min="3" maxlength="32" required>
+    <div class="flex category-icons">
+        <img src="./assets/img/false-x.png" class="false-x"> | <img src="./assets/img/checkmark.png" class="checkmark" onclick="addNewCategory()">
+    </div>`;
+}
+
+
+function colorsForNewCategory() {
+    let categoriesForColors = document.getElementById('categories-for-colors');
+    for(let i = 0; i < categoryColors.length; i++) {
+        categoryColor = categoryColors[i];
+        categoriesForColors.innerHTML += templateColorsForNewCategory(i, categoryColor);
+    }
+}
+
+function templateColorsForNewCategory(i, categoryColor) {
+    return /*html*/`
+    <span class="all-colors" style="background-color: ${categoryColor}" id="selected-color-${i}"></span>`;
 }
 
 
