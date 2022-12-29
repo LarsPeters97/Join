21d13dd00104e86294d8d07806208feb53003c5c
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2022-12-29 13:46:36 +0100

    added showcontacts

--- js/contacts.js
+++ js/contacts.js
@@ -5,6 +5,8 @@ setURL('https://gruppe-397.developerakademie.net/smallest_backend_ever');
 async function init() {
     await downloadFromServer();
     contacts = JSON.parse(backend.getItem("contacts")) || [];
+    console.log(contacts);
+    showContacts();
 }
 
 function showAddcontact() {
@@ -30,6 +32,19 @@ async function createContact() {
     let email = document.getElementById("input_email");
     let phone = document.getElementById("input_phone");
     contacts.push({name: name.value, email: email.value, phone: phone.value});
-    await  backend.setItem('users', JSON.stringify(contacts));
+    await  backend.setItem('contacts', JSON.stringify(contacts));
     window.location.href = './contact.html';
+}
+
+function showContacts(){
+    document.getElementById('contactsContainer').innerHtml = '';
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        console.log(contact);
+    document.getElementById('contactsContainer').innerHtml +=`
+    <div class="contact-card"> 
+    <p> ${contact.name} </p>
+    </div>`;
+        
+    }
 }
