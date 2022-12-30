a1506dfb8fa6824af914daa3e3c19d63bcff9b5a
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2022-12-30 14:19:44 +0100

    added show contact

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,17 @@
 let contacts = [];
-let colors = ['blue', 'red', 'orange', 'yellow', 'green', 'aquamarine', 'blueviolet', 'cadetblue'];
+let colors = [
+    "blue",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "aquamarine",
+    "blueviolet",
+    "cadetblue",
+];
+
+let currentcolor = 0;
+
 
 setURL("https://gruppe-397.developerakademie.net/smallest_backend_ever");
 
@@ -41,19 +53,59 @@ function showContacts() {
     document.getElementById("contactsContainer").innerHTML = "";
     for (let i = 0; i < contacts.length; i++) {
         const contact = contacts[i];
+        currentcolor ++;
+        let bgcolor = colors[currentcolor%contacts.length];
         splittedName = contact.name.split(" ");
-        document.getElementById("contactsContainer").innerHTML += showContactsHtml(contact);
-    
+        document.getElementById("contactsContainer").innerHTML +=
+            showContactsHtml(contact, splittedName, i, bgcolor);
     }
 }
 
-function showContactsHtml(contact){
-    return  `
-    <div class="contact-card">
-     <div class="initials-contact">${splittedName[0].charAt(0)}${splittedName[1].charAt(0)}</div>
+function showContactsHtml(contact, splittedName, i, bgcolor) {
+    return `
+    <div onclick="showContact(${i},${bgcolor})" class="contact-card">
+     <div style="background-color:${bgcolor}" class="initials-contact">${splittedName[0].charAt(0)}${splittedName[1].charAt(0)}</div>
      <div class="contact-card-text"> 
          <p> ${contact.name} </p>
-         <span> ${contact.email}</span>
+         <span> ${contact.email} </span>
      </div>
-    </div>`
+    </div>`;
+}
+
+function showContact(i, bgcolor) {
+    document.getElementById("contactAreaBody").innerHTML = "";
+    let contact = contacts[i];
+    let splittedName = contact.name.split(" ");
+    document.getElementById("contactAreaBody").innerHTML = 
+    showContactHtml(contact,splittedName, bgcolor);
+}
+
+function showContactHtml(contact,splittedName, bgcolor ){
+return `
+<div class="contactarea-body-name">
+    <div style="background-color:${bgcolor}" class="initials-contact-body">
+        ${splittedName[0].charAt(0)}${splittedName[1].charAt(0)}
+    </div>
+    <div class="container-name">
+        <h3>${contact.name} </h3>
+        <div class="add-task-link">
+            <img src="./assets/img/plus-lightblue.png" />
+            <span>Add Task</span>
+        </div>
+    </div>
+</div>    
+<div class="contactarea-body-contactinfo">
+    <span class="text-contact-info">Contact Information</span>
+    <div class="edit-link">
+        <img src="./assets/img/pen-blue.png" />
+        <span>Edit Contact</span>
+    </div>
+</div>
+<div class="contactarea-body-info">
+    <h5>Email</h5>
+    <span class="text-lightblue">${contact.email}</span>
+    <h5>Phone</h5>
+    <span>${contact.phone}</span>
+</div>
+`;
 }
